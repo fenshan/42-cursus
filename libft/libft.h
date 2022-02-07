2de59a87749475dad60da7f21c803ec22a947b0a
Fen Nicolas violant <fnicolas@c1r11s4.42madrid.com>
2022-02-07 22:39:50 +0100

    first commit

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnicolas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 20:11:12 by fnicolas          #+#    #+#             */
+/*   Updated: 2022/02/07 22:18:01 by fnicolas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+size_t	strlen(const char *s);
+void *	memset(void *b, int c, size_t len);
+
+#endif
